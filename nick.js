f2912dcd9838cf92ffdf492f514b3a964a87e312
nlert <neillertola@gmail.com>
2023-11-10 07:57:27 -0500

    clean up code

--- nick.js
+++ nick.js
@@ -1,37 +1,37 @@
-const input = document.getElementById("num-input"); // input
-const button = document.getElementById("enter"); // button
-const answer = document.getElementById("answer"); // p
+const input = document.getElementById("num-input");
+const button = document.getElementById("enter");
+const answer = document.getElementById("answer");
 
-function onlyNum(inputString) { // Test if input is a number
+function onlyNum(inputString) {
     return /^\d+$/.test(inputString);
 }
 
 button.addEventListener('click', function() { // 
-    const numVal = input.value; // Content added to input
+    const numVal = input.value;
 
-    answer.textContent = ""; // Clear answer content
-    let arr = []; // Array will contain fizz buzzes to display
+    answer.textContent = "";
+    let arr = [];
 
-    if (onlyNum(numVal) != true) { // Check if input is a number
+    if (onlyNum(numVal) != true) {
         answer.textContent = "Must enter a whole number";
-    } else if (numVal > 999) { // Check if number is < 1000
+    } else if (numVal > 999) {
         answer.textContent = "Number must be under 1000";
-    } else if (numVal < 1) { // Check if number is positive
+    } else if (numVal < 1) {
         answer.textContent = "Number must be positive";
     } else {
         for (let i = 1; i <= numVal; i++) {
-            if (i % 3 === 0 && i % 5 === 0) { // Is num divisible by 3 and 5
-                arr.push(" fizz buzz");
-            } else if (i % 3 === 0) { // Is num divisible by 3
-                arr.push(" fizz");
-            } else if (i % 5 === 0) { // Is num divisible by 5
-                arr.push(" buzz");
-            } else { // Add the number to the array
-                arr.push(" " + i);
+            if (i % 3 === 0 && i % 5 === 0) {
+                arr.push("fizzbuzz");
+            } else if (i % 3 === 0) {
+                arr.push("fizz");
+            } else if (i % 5 === 0) {
+                arr.push("buzz");
+            } else {
+                arr.push(i);
             }
         }
 
-        answer.textContent = arr; // Display the answer
+        answer.textContent = arr.join(" ");
     }
 
     answer.classList.add("border");
